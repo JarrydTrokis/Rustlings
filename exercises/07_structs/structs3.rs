436ae0430dc5101aad04f15b179275e5311ed716
Jarryd <jarryd.jay@gmail.com>
2024-07-24 16:59:13 +0100

    Getting into enums

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -27,7 +27,7 @@ impl Package {
     fn is_international(&self) -> bool {
         // TODO: Read the tests that use this method to find out when a package
         // is considered international.
-        self.sender_country == self.recipient_country
+        self.sender_country != self.recipient_country
     }
 
     // TODO: Add the correct return type to the function signature.

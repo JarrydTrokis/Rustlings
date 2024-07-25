ab3c77c42c1d4d0546a5516fe9a427293f77178b
Jarryd Trokis <jarryd.jay@gmail.com>
2024-07-25 20:21:18 +0100

    Finished strings

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -1,6 +1,7 @@
 // TODO: Fix the compiler error without changing the function signature.
 fn current_favorite_color() -> String {
-    "blue"
+    // String::from("blue")
+    "blue".to_string()
 }
 
 fn main() {

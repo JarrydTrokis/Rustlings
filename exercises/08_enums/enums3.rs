cbd9958940df4b33bf9c51d1b6c37a8151720957
Jarryd <jarryd.jay@gmail.com>
2024-07-25 17:42:40 +0100

    Enums finished

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,6 +5,11 @@ struct Point {
 
 enum Message {
     // TODO: Implement the message variant types based on their usage below.
+    Resize { width: u64, height: u64 },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 struct State {
@@ -42,6 +47,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(Point { x, y }) => self.move_position(Point { x, y }),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+        }
     }
 }
 

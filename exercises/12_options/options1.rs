102d6242c01c44fbb3dcf002ca05031d8077b297
Jarryd <jarryd.jay@gmail.com>
2024-08-26 16:03:46 +0100

    Moving onto 'handling errors'

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,13 @@
 // `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day < 22 {
+        Option::Some(5)
+    } else if hour_of_day <= 23 {
+        Option::Some(0)
+    } else {
+        Option::None
+    }
 }
 
 fn main() {
@@ -18,9 +25,9 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
-
-        assert_eq!(icecreams, 5); // Don't change this line.
+        if let Some(icecreams) = maybe_icecream(12) {
+            assert_eq!(icecreams, 5); // Don't change this line.
+        }
     }
 
     #[test]
--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -7,10 +7,13 @@ mod tests {
     #[test]
     fn simple_option() {
         let target = "rustlings";
+        // convert this to an Optional Type
         let optional_target = Some(target);
 
         // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        if let Some(word) = optional_target {
+            // assert that the thing we converted is equal to the
+            // source we converted it from
             assert_eq!(word, target);
         }
     }
@@ -29,9 +32,14 @@ mod tests {
         // TODO: Make this a while-let statement. Remember that `Vec::pop()`
         // adds another layer of `Option`. You can do nested pattern matching
         // in if-let and while-let statements.
-        integer = optional_integers.pop() {
-            assert_eq!(integer, cursor);
-            cursor -= 1;
+
+        while let Some(integer) = optional_integers.pop() {
+            // ℹ️  Interesting to note that we can reuse the variable name
+            // to do nested destructuring in the while-let & if-let statements
+            if let Some(integer) = integer {
+                assert_eq!(integer, cursor);
+                cursor -= 1;
+            }
         }
 
         assert_eq!(cursor, 0);

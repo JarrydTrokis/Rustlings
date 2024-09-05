531238743fe7421eaa32461af84177900969c60a
Jarryd <jarryd.jay@gmail.com>
2024-09-05 17:08:36 +0100

    Finished - onto testing

--- exercises/16_lifetimes/lifetimes1.rs
+++ exercises/16_lifetimes/lifetimes1.rs
@@ -3,8 +3,16 @@
 // going out of scope before it is used. Remember, references are borrows and do
 // not own their own data. What if their owner goes out of scope?
 
-// TODO: Fix the compiler error by updating the function signature.
-fn longest(x: &str, y: &str) -> &str {
+// ℹ️  I'm still not 100% sure what the lifetime definition is doing here - but
+// from what I gather, I think it's something to help the compiler understand
+// which REFERENCE (aka: "&" value) the function should keep around. We need to
+// annotate the function with a 'generic-like' argument that is a lifetime.
+// This lifetime will help the compiler understand that we've got some parameters
+// coming in that must be around as long as the function is - because if not
+// the program will invalidate that memory address, meaning that the variable
+// is no longer available.
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {

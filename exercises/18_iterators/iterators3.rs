ebbdbdf29f25f2959ee9d34ff81ca22c93badcf4
Jarryd <jarryd.jay@gmail.com>
2024-09-09 14:13:15 +0100

    Learning all the time...

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -1,3 +1,5 @@
+use std::i64;
+
 #[derive(Debug, PartialEq, Eq)]
 enum DivisionError {
     // Example: 42 / 0
@@ -11,22 +13,76 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    if a == i64::MIN && b == -1 {
+        return Err(DivisionError::IntegerOverflow);
+    }
+
+    if a % b != 0 {
+        return Err(DivisionError::NotDivisible);
+    }
+
+    Ok(a / b)
 }
 
-// TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // let mut res: Vec<i64> = vec![];
+    //
+    // for val in division_results {
+    //     if let Ok(v) = val {
+    //         res.push(v)
+    //     }
+    // }
+    // Ok(res)
+    division_results.collect()
 }
 
-// TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+fn list_of_results() -> Vec<Result<i64, DivisionError>> {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // ℹ️  My solution worked but it was *very* basic.
+    // Looking at the solution, the .collect() function appears to have
+    // this sort of behaviour built-in.
+    //
+    // let mut res: Vec<Result<i64, ()>> = vec![];
+    //
+    // for val in division_results {
+    //     match val {
+    //         Ok(v) => res.push(Ok(v)),
+    //         _ => (),
+    //     }
+    // }
+    // res
+    division_results.collect()
 }
+
+// 📝 This is important when working with Iterators!
+// Transforms an iterator into a collection.
+//
+// `collect()` can take anything iterable, and turn it into a relevant
+// collection. This is one of the more powerful methods in the standard
+// library, used in a variety of contexts.
+//
+// The most basic pattern in which `collect()` is used is to turn one
+// collection into another. You take a collection, call [`iter`] on it,
+// do a bunch of transformations, and then `collect()` at the end.
+//
+// `collect()` can also create instances of types that are not typical
+// collections. For example, a [`String`](https://doc.rust-lang.org/stable/core/iter/std/string/struct.String.html) can be built from [`char`]s,
+// and an iterator of [`Result<T, E>`](https://doc.rust-lang.org/stable/core/result/enum.Result.html) items can be collected
+// into `Result<Collection<T>, E>`. See the examples below for more.
+//
+// Because `collect()` is so general, it can cause problems with type
+// inference. As such, `collect()` is one of the few times you'll see
+// the syntax affectionately known as the 'turbofish': `::<>`. This
+// helps the inference algorithm understand specifically which collection
+// you're trying to collect into.
 
 fn main() {
     // You can optionally experiment here.
